c834e34cd98d39133b8a35af1047c84ac53641ce
kaustavsarkar14 <sarkar.kaustav14@gmail.com>
2023-11-15 14:37:07 +0530

    first commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,44 @@
 
-import React from "react";
+import React, { useState } from "react";
 import './../styles/App.css';
 
+
 const App = () => {
+  const [data, setData] = useState([])
+  function getData(){
+    fetch('https://reqres.in/api/users')
+    .then(data=>data.json())
+    .then(data=>setData(data.data))
+  }
   return (
     <div>
         {/* Do not remove the main div */}
+        <h1>Blue Whales</h1>
+        <button onClick={getData} className="btn" >Get User List</button>
+        <table style={{width:"100%"}}>
+          <thead>
+            <tr >
+              <th>First Name</th>
+              <th>Last Name</th>
+              <th>Email</th>
+              <th>Avatar</th>
+            </tr>
+          </thead>
+          <tbody>
+            {
+              data.map(el=>{
+                return (
+                  <tr key={el.id}>
+                    <td>{el.first_name}</td>
+                    <td>{el.last_name}</td>
+                    <td>{el.email}</td>
+                    <td><img src={el.avatar} alt="" /></td>
+                  </tr>
+                )
+              })
+            }
+          </tbody>
+        </table>
     </div>
   )
 }
