8333357df5faa59152f6df03361683fbaa2ad645
kaustavsarkar14 <sarkar.kaustav14@gmail.com>
2023-11-15 14:46:09 +0530

    new

--- src/components/App.js
+++ src/components/App.js
@@ -37,7 +37,7 @@ const App = () => {
                 )
               }))
               :
-              <tr><td>No data found</td></tr>
+              <tr><td>No data found to display.</td></tr>
             }
           </tbody>
         </table>
