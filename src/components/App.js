62779ca7fdd1817afe1f2259798d2888d6d97a90
kaustavsarkar14 <sarkar.kaustav14@gmail.com>
2023-11-16 20:49:23 +0530

    new

--- src/components/App.js
+++ src/components/App.js
@@ -1,46 +1,50 @@
 
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import './../styles/App.css';
 
-
 const App = () => {
-  const [data, setData] = useState(null)
-  function getData(){
+  const [people, setPeople] = useState([])
+
+  function getData() {
     fetch('https://reqres.in/api/users')
-    .then(data=>data.json())
-    .then(data=>setData(data.data))
+      .then(data => data.json())
+      .then(data => setPeople(data.data))
   }
+
   return (
     <div>
-        {/* Do not remove the main div */}
+      {/* Do not remove the main div */}
+      <header className="nav">
         <h1>Blue Whales</h1>
-        <button onClick={getData} className="btn" >Get User List</button>
-        <table style={{width:"100%"}}>
-          <thead>
-            <tr >
-              <th>First Name</th>
-              <th>Last Name</th>
-              <th>Email</th>
-              <th>Avatar</th>
-            </tr>
-          </thead>
-          <tbody>
-            {data?
-              (data.map(el=>{
+        <button onClick={getData} >Get User List</button>
+      </header>
+      <table>
+        <thead>
+          <tr>
+            <th>First Name</th>
+            <th>Last Name</th>
+            <th>Email</th>
+            <th>Avatar</th>
+          </tr>
+        </thead>
+        <tbody>
+          {
+            people.length == 0 ? "No data found to display."
+              :
+              people.map((people, i) => {
                 return (
-                  <tr key={el.id}>
-                    <td>{el.first_name}</td>
-                    <td>{el.last_name}</td>
-                    <td>{el.email}</td>
-                    <td><img src={el.avatar} alt="" /></td>
+                  <tr key={i} >
+                    <td>{people.first_name}</td>
+                    <td>{people.last_name}</td>
+                    <td>{people.email}</td>
+                    <img src={people.avatar} alt="" />
                   </tr>
                 )
-              }))
-              :
-              <tr><td>No data found to display.</td></tr>
-            }
-          </tbody>
-        </table>
+              })
+
+          }
+        </tbody>
+      </table>
     </div>
   )
 }
